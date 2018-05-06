42ce946e73a11eea7d2cbe252c88f6927615c2f2
Marcos Barbosa <barbosa.mg@gmail.com>
2018-05-06 20:52:10 -0300

    Criando tarefas build-img, build-js, build-html criando uma tarefa default para que possamos chamá-la e ela executa as outras, conforme definido no gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace');
 
+
+gulp.task('default', ['copy'], function() {
+    gulp.start('build-img', 'build-js', 'build-html');
+});
 
 gulp.task('copy', ['clean'], function() {
-    return gulp.src('src/**/*')
+    gulp.src('src/**/*')
         .pipe(gulp.dest('dist'));
 });
 
@@ -13,10 +19,24 @@ gulp.task('clean', function() {
         .pipe(clean());
 });
     
-gulp.task('build-img', ['copy'], function() {
+gulp.task('build-img', function() {
         
     gulp.src('src/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
 
+});
+
+gulp.task('build-js', function() {
+    gulp.src(['dist/js/jquery.js', 'dist/js/home.js', 'dist/js/produto.js'])
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('build-html', function() {
+    gulp.src('dist/**/*.html')
+        .pipe(htmlReplace({
+            js: 'js/all.js'
+        }))
+        .pipe(gulp.dest('dist'));
 });
