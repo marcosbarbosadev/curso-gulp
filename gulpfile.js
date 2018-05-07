184be015d5e2f1e0546782f1975011c488f28b19
Marcos Barbosa <barbosa.mg@gmail.com>
2018-05-06 22:42:45 -0300

    Usando jshint e csslint para monitorar erros sempre que salvar um arquivo js ou css respectivamente

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
-var gulp = require('gulp'),
-    imagemin = require('gulp-imagemin'),
-    clean = require('gulp-clean'),
-    concat = require('gulp-concat'),
-    htmlReplace = require('gulp-html-replace'),
-    uglify = require('gulp-uglify'),
-    usemin = require('gulp-usemin'),
-    cssmin = require('gulp-cssmin'),
-    browserSync = require('browser-sync');
+var gulp = require('gulp')
+    ,imagemin = require('gulp-imagemin')
+    ,clean = require('gulp-clean')
+    ,concat = require('gulp-concat')
+    ,htmlReplace = require('gulp-html-replace')
+    ,uglify = require('gulp-uglify')
+    ,usemin = require('gulp-usemin')
+    ,cssmin = require('gulp-cssmin')
+    ,browserSync = require('browser-sync')
+    ,jshint = require('gulp-jshint')
+    ,jshintStylish = require('jshint-stylish')
+    ,csslint = require('gulp-csslint');
 
 
 gulp.task('default', ['copy'], function() {
@@ -46,5 +49,18 @@ gulp.task('server', function() {
             baseDir: 'src'
         }
     });
+
+    gulp.watch('src/js/**/*').on('change', function(event) {
+        gulp.src(event.path)
+            .pipe(jshint())
+            .pipe(jshint.reporter(jshintStylish));
+    });
+
+    gulp.watch('src/css/**/*').on('change', function(event) {
+        gulp.src(event.path)
+            .pipe(csslint())
+            .pipe(csslint.reporter());
+    });
+
     gulp.watch('src/**/*').on('change', browserSync.reload);
 });
